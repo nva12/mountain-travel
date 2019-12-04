5628ec4a56e5ef949c2e950df6493bf96b885e7b
nva12 <nvallee.a7@gmail.com>
2019-12-04 10:00:58 +0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadein').each( function(i){
+            
+            var threshold_of_element = $(this).offset().top + 0.3*$(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > threshold_of_element ){
+                $(this).animate({'opacity':'1'}, 1500);
+            }
+            
+        });
+        $('.fadeinleft').each( function(i){
+            
+            var threshold_of_element = $(this).offset().top + 0.3*$(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > threshold_of_element ){
+                $(this).animate({'opacity':'1','margin-left':'0px'}, 1500);
+            }
+            
+        });
+        $('.fadeinright').each( function(i){
+            
+            var threshold_of_element = $(this).offset().top + 0.3*$(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > threshold_of_element ){
+                $(this).animate({'opacity':'1','margin-right':'0px'}, 1500);
+            }
+            
+        });  
+    });
+});
